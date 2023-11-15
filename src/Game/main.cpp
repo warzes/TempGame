aacda6baa2914e6877db66b189b3a9144a27e741
warzes <warzes@mail.ru>
2023-11-15 15:46:20 +1000

    -----

--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -1,5 +1,58 @@
 ﻿#include "stdafx.h"
 #include "GameApp01.h"
+
+
+поискать тот воксельный редактор на юнити
+сделать из вокселей такие блоки - https://kenney.nl/assets/mini-dungeon
+	а потом дальше
+	вот еще варианты блоков - https://devilsworkshop.itch.io/big-pixel-isometric-block-pack-free-2d-sprites
+	https://devilsworkshop.itch.io/essential-low-poly-isometric-3d-block-and-hex-pack
+	https://shallow-lagoon.itch.io/mega-isometric-block-pack
+	https://devilsworkshop.itch.io/essential-low-poly-isometric-3d-block-and-hex-pack
+
+найти dnd картк/прикл и в trenchbroom
+
+для тек проекта
+взять вот этот ассет - https://kenney.nl/assets/mini-dungeon
+не забыть про voxel farm island
+еще для общих идей тайлов - https://maxparata.itch.io/monogon-isometricdungeon
+еще красиво - https://maxparata.itch.io/medievaltown-monogon
+возможно не в магикавоселе, а в тренчбруме или скетчап
+для опенворда - https://krishna-palacio.itch.io/minifantasy-tiny-overworld
+
+еще и это - https://www.youtube.com/watch?v=Q1yBrmIf1Yw
+	https://www.youtube.com/watch?v=BhX48pe-6zc
+	
+	https://itch.io/game-assets/tag-medieval/tag-voxel
+	
+вид сверху как дьябло???
+
+
+
+по https://maxparata.itch.io/medievaltown-monogon
+	если разделять, то то вот пример - https://filipevoxel.itch.io/voxel-house-contruction
+	
+а для персов - https://www.youtube.com/watch?v=_DkgWy_KscU и https://www.youtube.com/watch?v=U7IjOxc4skg
+
+
+https://shallow-lagoon.itch.io/mega-isometric-block-pack
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //
 //СОЗДАТЬ ПРОЕКТ
 //
